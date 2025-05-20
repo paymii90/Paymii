c103b1c677e420472231320e9d54b68df234ec48
lordsonfafa <lordsonborngreat@gmail.com>
2025-05-20 13:28:01 -0700

    new

---
+++ frontend/src/screens/progress1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text, Image, StyleSheet
+} from 'react-native'
+
+const Progress1 =()=>{
+    return <View>
+<Image source={required('../../assets/SecureAccount.png')} />
+    </View>
+}
+
+const styles = StyleSheet.create({})
+
+export default Progress1
