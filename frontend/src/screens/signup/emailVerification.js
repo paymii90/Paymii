603bc37d042e6cc1d1493858b8481a1e8aada2ba
lordsonfafa <lordsonborngreat@gmail.com>
2025-05-20 14:14:14 -0700

    updating paths

--- frontend/src/screens/signup/emailVerification.js
+++ frontend/src/screens/signup/emailVerification.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
-import Button from "../Components/Button";
+import Button from "../../Components/Button";
 
 const emailVerification = ({ navigation }) => {
   return (
