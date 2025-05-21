f7c27c06c84f01c94b301e9e2727eb1fef43d7b7
4Ts-code <eo8407272@gmail.com>
2025-05-21 03:10:13 +0000

    setup done

--- frontend/src/navigation/TabNavigator.js
+++ frontend/src/navigation/TabNavigator.js
@@ -10,22 +10,79 @@ import TransactionsScreen from "../screens/MainScreens/TransactionsScreen";
 import ExploreScreen from "../screens/MainScreens/ExploreScreen";
 import SettingsScreen from "../screens/MainScreens/SettingsScreen";
 
+//import icons
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { View } from "react-native";
+
 const TabNavigator = createBottomTabNavigator(
   {
-    Home: Home,
-    Portfolio: PortfolioScreen,
-    Transactions: TransactionsScreen,
-    Explore: ExploreScreen,
-    Profile: SettingsScreen,
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialIcons name="home" size={30} color={tintColor} />
+        ),
+      },
+    },
+    Portfolio: {
+      screen: PortfolioScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialIcons name="pie-chart" size={30} color={tintColor} />
+        ),
+      },
+    },
+    Transactions: {
+      screen: TransactionsScreen,
+      navigationOptions: {
+        tabBarLabel: () => null,
+        tabBarIcon: ({ tintColor }) => (
+          <View
+            style={{ justifyContent: "center", alignItems: "center" }}
+          >
+            <MaterialIcons name="receipt" size={45} color={tintColor} />
+          </View>
+        ),
+      },
+    },
+    Explore: {
+      screen: ExploreScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialIcons name="explore" size={30} color={tintColor} />
+        ),
+      },
+    },
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialIcons name="person" size={30} color={tintColor} />
+        ),
+      },
+    },
   },
   {
     tabBarOptions: {
-      activeTintColor: "#007AFF",
-      inactiveTintColor: "gray",
+      activeTintColor: "##052644",
+      inactiveTintColor: "rgb(129, 129, 129)",
       style: {
-        height: 60,
-        paddingBottom: 5,
-        backgroundColor: "#fff",
+        // backgroundColor: "#fff",
+        // borderTopWidth: 0.5,
+        // borderTopColor: "#ccc",
+        // paddingBottom: 10,
+        //  paddingTop: 10,
+        // marginBottom: 10,
+        paddingVertical: 10, // adds vertical padding inside the tab bar
+        paddingHorizontal: 20, // optional, horizontal padding
+        height: 60, // you can increase the height if you want more space
+        backgroundColor: "#fff", // customize background color too if you want
+        borderTopWidth: 1,
+        borderTopColor: "#ddd",
+      },
+      labelStyle: {
+        fontSize: 13,
+        marginBottom: 5,
       },
     },
   }
--- frontend/src/screens/MainScreens/ExploreScreen.js
+++ frontend/src/screens/MainScreens/ExploreScreen.js
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import React from "react";
 
 const ExploreScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>ExploreScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ExploreScreen
+export default ExploreScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: "#fff",
+    justifyContent: 'center',
+  },
+});
