a63ecddd61ec4045d38711ba7e1c62f6d28f9330
4Ts-code <eo8407272@gmail.com>
2025-06-02 12:13:51 +0000

    setup navigation for sub screens

---
+++ frontend/src/screens/SubScreens/HomeSub/SellAssets/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SellAssets = () => {
+  return (
+    <View  style={{alignItems: 'center',justifyContent: 'center', flex: 1}}>
+      <Text>SellAssets</Text>
+    </View>
+  );
+};
+
+export default SellAssets;
+
+const styles = StyleSheet.create({});
