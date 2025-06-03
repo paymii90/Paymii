945e90456a8cb36d5a659bea65671411a07f3739
4Ts-code <eo8407272@gmail.com>
2025-06-03 16:44:14 +0000

    home nav

---
+++ frontend/src/screens/SubScreens/HomeSub/Deposit/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Deposit = () => {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+      <Text>Deposit</Text>
+    </View>
+  );
+};
+
+export default Deposit;
+
+const styles = StyleSheet.create({});
---
+++ frontend/src/screens/SubScreens/HomeSub/Send/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SendCrypto = () => {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+      <Text>SendCrypto</Text>
+    </View>
+  );
+};
+
+export default SendCrypto;
+
+const styles = StyleSheet.create({});
